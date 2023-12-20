595f529ee2a6c102b30be7aa7e28b1fcc3010118
Aziz Alfa <azizalfauzi65@gmail.com>
2023-12-20 10:59:36 +0700

    *REACT APP - React Fragment

--- pizza-menu-app/src/index.js
+++ pizza-menu-app/src/index.js
@@ -73,12 +73,19 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our Menu</h2>
+
       {numPizza > 0 ? (
-        <ul className="pizzas">
-          {pizzas.map((pizza) => (
-            <Pizza pizzaObj={pizza} key={pizza.name} />
-          ))}
-        </ul>
+        <>
+          <p>
+            Authentice Italian cuisine. 6 creative dishes to choose from. All
+            from our stone, all organic, all delicious
+          </p>
+          <ul className="pizzas">
+            {pizzas.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
       ) : (
         <p>We're still working on our menu . Please come back later!</p>
       )}
